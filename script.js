3f5c0cc591e86254c70f205f0a446fb26f73b42a
gaurav-knp <gauravguptak142@gmail.com>
2025-07-30 13:46:07 +0530

    Added script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let selectopt = document.querySelectorAll("select");
+let btn = document.querySelector("button");
 for(select of selectopt){
     for(code in countryList){
         let opt =document.createElement("option");
@@ -10,7 +11,7 @@ for(select of selectopt){
             opt.selected = "selected";
     }
         select.appendChild(opt);
-        
+    } 
     select.addEventListener("change", (evt)=>{
         updateFlag(evt.target);
     });
@@ -22,5 +23,14 @@ const updateFlag =(element)=>{
     element.parentElement.querySelector("img").src = newSrc;
 };
 
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    let amount = document.querySelector("input").value;
+    if(amount === "" || isNaN(amount) || amount <= 0){
+       amount = 1;
+       amount.value = 1;
+    }
+    
+});
 
 
